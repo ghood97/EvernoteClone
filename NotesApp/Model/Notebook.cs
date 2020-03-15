4a1fab94253b9e4db2d22934f29bffc1c4dbaec5
ghood97 <griffinhood@verizon.net>
2020-03-15 00:15:15 -0400

    Build models and implement INotifyPropertyChanged

---
+++ NotesApp/Model/Notebook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NotesApp.Model
+{
+    public class Notebook : INotifyPropertyChanged
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set
+			{
+				id = value;
+				OnPropertyChanged("Id");
+			}
+		}
+
+		private int userId;
+
+		public int UserId
+		{
+			get { return UserId; }
+			set
+			{
+				UserId = value;
+				OnPropertyChanged("UserId");
+			}
+		}
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set
+			{
+				name = value;
+				OnPropertyChanged("Name");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+
+	}
+}
---
+++ NotesApp/Model/User.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NotesApp.Model
+{
+    public class User : INotifyPropertyChanged
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set
+			{
+				id = value;
+				OnPropertyChanged("Id");
+			}
+		}
+
+		private string firstName;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set
+			{
+				firstName = value;
+				OnPropertyChanged("FirstName");
+			}
+		}
+
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+			set
+			{
+				lastName = value;
+				OnPropertyChanged("LastName");
+			}
+		}
+
+		private string userName;
+
+		public string UserName
+		{
+			get { return userName; }
+			set
+			{
+				userName = value;
+				OnPropertyChanged("UserName");
+			}
+		}
+
+		private string email;
+
+		public string Email
+		{
+			get { return email; }
+			set
+			{
+				email = value;
+				OnPropertyChanged("Email");
+			}
+		}
+
+		private string password;
+
+		public string Password
+		{
+			get { return password; }
+			set
+			{
+				password = value;
+				OnPropertyChanged("Password");
+			}
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+
+
+
+
+
+	}
+}
